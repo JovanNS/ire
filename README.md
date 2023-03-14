2fb9b4dba1e7d5c8062c1f05a9b31be77cbc2abd
jmatic <jmatic@UBUNTU22.04.myguest.virtualbox.org>
2023-03-14 03:35:25 -0700

    Basing testing, README changes

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-localhost/api/v1/real-estate-entities?filter[price_between]=8000000,8200000&filter[address]=a&filter[size]=2676&filter[number_of_rooms]=5
 
+# SETUP
+```
+ ./vendor/bin/sail up -d
+```
+
+# COMMENTS
+The task is pretty basic but it covers some of the aspects of production ready code
+```
+There are tests tho very, very basic (written for the sake of writting tests), but ofc every api should have all of it's endpoints tested (but also unit tests)
+Api is versioned
+There are transformation layers (api resources)
+Relathionships are eager loaded so no n+1
+Foreign keys used where necessary
+Requests are validated and only the validated data is used when injecting into the DB
+Api is following a JSON API standard (as close as the package that i used is doing it but it should be very close)
+```
+# EXAMPLES
+
+GET 
+```
+localhost/api/v1/real-estate-entities?filter[price_between]=8000000,8200000&filter[address]=a&filter[size]=2676&filter[number_of_rooms]=5
+```
+
+POST 
+
+```
+localhost/api/v1/real-estate-entities
+
+{
+    "address":"address1",
+    "price":100,
+    "latitude": 44.787197,
+    "longitude": 20.457273,
+    "number_of_rooms": 3,
+    "size": 60.5,
+    "type_id": 1
+}
+```
+
+## BONUS
+GET 
+```
+localhost/api/v1/real-estate-entities?filter[price_between]=8000000,8200000&filter[address]=a&filter[size]=2676&filter[number_of_rooms]=5
+```
+
+# TODO
+```
+Question the db structure
+Improve validation rules
+Authorization and authentication
+Proper testing for every endpoint and scenario
+```
---
+++ tests/Feature/RealEstateEntityTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\RealEstateEntity;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RealEstateEntityTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_get_entities_available(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+
+    public function test_can_create_entity(): void
+    {
+        $response = $this->post('/api/v1/real-estate-entities', [
+            "address" => "address1",
+            "price" => 100,
+            "latitude" => 44.787197,
+            "longitude" => 20.457273,
+            "number_of_rooms" => 3,
+            "size" => 60.5,
+            "type_id" => 1
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function test_cant_create_entity_when_validation_is_not_passing(): void
+    {
+        $response = $this->post('/api/v1/real-estate-entities', [
+            "price" => "dsada",
+            "latitude" => 44.787197,
+            "longitude" => 20.457273,
+            "number_of_rooms" => 3,
+            "size" => 60.5,
+            "type_id" => 1
+        ], [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_can_update_entity(): void
+    {
+        $entity = RealEstateEntity::factory()->create();
+
+        $response = $this->patch("/api/v1/real-estate-entities/{$entity->id}", [
+            'address' => "Updated"
+        ]);
+
+        $entity->refresh();
+
+        $response->assertStatus(200);
+
+        $this->assertEquals($entity->address, "Updated");
+    }
+}
